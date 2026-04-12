e4a6d076ef5c56d2c710a8ea14e8b343f1d61498
John Ogungbure <jogungbure@gmail.com>
2026-04-12 18:18:39 +0100

    chore: fixed cta shape

--- src/data/baloshWpImages.ts
+++
@@ -1,4 +0,0 @@
-import { trustedClients } from "@/data/trustedClients"
-
-/** Local trusted-client logo paths (`public/assets/img/logo/trusted-clients/`). */
-export const BALOSH_WP_CONTENT_IMAGES: readonly string[] = trustedClients.map((c) => c.logoUrl)
